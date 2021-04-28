fd441435b76ce08508ef2376bf33bc692ac050a4
yousirong <80014277+yousirong@users.noreply.github.com>
2021-04-28 15:29:39 +0900

    20210428 report update

--- 2-oop/bunsu/bunsu.js
+++ 2-oop/bunsu/bunsu.js
@@ -3,17 +3,7 @@ class Comparable {
     return null
   }
 }
-function gcd(n, m) {
-  // Euclids Greatest Common Divisor algorithm.
-  while (n !== m) {
-    if (n < m) {
-      m = m - n
-    } else {
-      n = n - m
-    }
-  }
-  return n
-}
+
 class Bunsu extends Comparable {
   constructor(top, bot) {
     super()
@@ -27,7 +17,17 @@ class Bunsu extends Comparable {
   compareTo(right) {
     return this.bunja * right.bunmo - this.bunmo * right.bunja
   }
-
+  static gcd(n, m) {
+    // Euclids Greatest Common Divisor algorithm.
+    while (n !== m) {
+      if (n < m) {
+        m = m - n
+      } else {
+        n = n - m
+      }
+    }
+    return n
+  }
   normalize() {
     // Normalize fraction by
     // (a) moving sign to bunja
@@ -41,12 +41,10 @@ class Bunsu extends Comparable {
       sign = -sign
       this.bunmo = -this.bunmo
     }
-    let d = 1
-    if (this.bunja > 0) {
-      d = gcd(this.bunja, this.bunmo)
-    }
+    let d = this.bunja !== 0 ? Bunsu.gcd(this.bunja, this.bunmo) : 1
     this.bunja = sign * (this.bunja / d)
     this.bunmo = this.bunmo / d
+    //console.log(`sign=${sign} d=${d} bunja=${this.bunja} bunmo=${this.bunmo}`)
   }
   plus(right) {
     return new Bunsu(this.bunja * right.bunmo + right.bunja * this.bunmo, this.bunmo * right.bunmo)
@@ -73,48 +71,48 @@ class Bunsu extends Comparable {
     return this
   }
 }
-{
-  let a = new Bunsu(0, 1) // Value is 0/1
-  let b = new Bunsu(4, 1) // Value is 4/1
-  let c = new Bunsu(6, 8) // Value is 6/8, which is converted to 3/4
-  console.log(`Constructed values ${a} ${b} ${c}`)
 
-  let d = c // d reference c(3/4) instance(aliasing)
-  c.next()
-  console.log(`Value of c is ${c}`)
-  console.log(`Value of d is ${d}`)
+let a = new Bunsu(0, 1) // Value is 0/1
+let b = new Bunsu(4, 1) // Value is 4/1
+let c = new Bunsu(6, 8) // Value is 6/8, which is converted to 3/4
+console.log(`Constructed values ${a} ${b} ${c}`)
 
-  // Test arithmetic instructions
-  // d = b + c
-  d = b.plus(c)
-  console.log(`4 + 3/4 is ${d}`)
-  // d = b - c
-  d = b.minus(c)
-  console.log(`4 - 3/4 is ${d}`)
-  //let e = (b + (-c));
-  let e = b.plus(c.negative())
-  console.log(`Done with negation e = ${e}`)
+let d = c // d reference c(3/4) instance(aliasing)
+c.next()
+console.log(`Value of c is ${c}`)
+console.log(`Value of d is ${d}`)
 
-  if (d === e) {
-    console.log(`Subtraction test successful.`)
-  } else {
-    console.log(`Subtraction test failed.`)
-  }
+// Test arithmetic instructions
+c = new Bunsu(3, 4)
+// d = b + c
+d = b.plus(c)
+console.log(`4 + 3/4 is ${d}`)
+// d = b - c
+d = b.minus(c)
+console.log(`4 - 3/4 is ${d}`)
+//let e = (b + (-c));
+let e = b.plus(c.negative())
+console.log(`Done with negation e = ${e}`)
 
-  a = new Bunsu(6, 8)
-  b = new Bunsu(7, 8)
-  if (a.compareTo(b) < 0) {
-    console.log(`Compare successful. a<=>b = ${a.compareTo(b)}`)
-  }
-  if (a.compareTo(b) <= 0) {
-    console.log(`a<=b = ${a.compareTo(b) <= 0}`)
-  }
-
-  let h = new Bunsu(0, 1)
-  for (let i = 1; i <= 30; ++i) {
-    //h = h + new Bunsu(1,i);
-    h = h.plus(new Bunsu(1, i))
-    console.log(` 1/1+...+1/${i} = ${h}`)
-  }
-  console.log(` 1/1+...+1/30 = ${h}`)
+if (d === e) {
+  console.log(`Subtraction test successful.`)
+} else {
+  console.log(`Subtraction test failed.`)
 }
+
+a = new Bunsu(6, 8)
+b = new Bunsu(7, 8)
+if (a.compareTo(b) < 0) {
+  console.log(`Compare successful. a<=>b = ${a.compareTo(b)}`)
+}
+if (a.compareTo(b) <= 0) {
+  console.log(`a<=b = ${a.compareTo(b) <= 0}`)
+}
+
+let h = new Bunsu(0, 1)
+for (let i = 1; i <= 30; ++i) {
+  //h = h + new Bunsu(1,i);
+  h = h.plus(new Bunsu(1, i))
+  console.log(` 1/1+...+1/${i} = ${h}`)
+}
+console.log(` 1/1+...+1/30 = ${h}`)
---
+++ report-2/report-2.md
@@ -0,0 +1,12 @@
+# Report 2
+
+## Due Date 5월 12일(수)
+
+1. app.js, phone-book.js 수정
+2. remove, update 의 keyObj이 name만이 아닌 email이나, phone이 가능하게 수정
+3. 데이터 추가 -- 더미데이터 3명 {name, email, phone}, 모든 학생이 다름.
+4. 데이터 추가 -- 마지막, 학생 본인 name(id, 이름), email(실제), phone(가상)
+
+### 제출 -- eclass에 자신의 report-2 folder를 zip 해서 올림
+
+### 다른 부가 리포트 필요 없음
