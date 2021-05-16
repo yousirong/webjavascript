608ffafc4e2bf817e10c07a72cd31b0b0a1c6793
yousirong <80014277+yousirong@users.noreply.github.com>
2021-05-17 03:48:18 +0900

    dd

--- report-3/sort/연습.js
+++
@@ -1,8 +0,0 @@
-
-  str1 = "CL-2";
-  str2 = "HE-2";
-  str3 = "DI-2";
-  console.log(str1);
-  console.log(str1.compareTo(str2));
-  console.log(str2.compareTo(str3));
-  console.log(str3.compareTo(str1));
